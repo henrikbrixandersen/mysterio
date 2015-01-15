13daa0bee65cbc502eadcc9e0f7b1f1289f31847
Henrik Brix Andersen <henrik@brixandersen.dk>
2015-01-15 23:12:53 +0100

    Improve the pixel change effect.

--- Mysterio/MysterioView.m
+++ Mysterio/MysterioView.m
@@ -11,7 +11,7 @@
 
 #pragma mark - Definitions
 
-#define FRAME_RATE 10.0
+#define FRAME_RATE 5.0
 
 #define PIXEL_COUNT_MAX 22
 #define PIXEL_BORDER 0.10
@@ -24,47 +24,18 @@
 @property (strong, nonatomic) NSArray *bluePixels;
 @property (strong, nonatomic) NSArray *whitePixels;
 
-+ (NSArray*)createPixelsWithSize:(NSInteger)size color:(NSColor*)color rows:(NSInteger)rows columns:(NSInteger)columns xOffset:(NSInteger)xOffset yOffset:(NSInteger)yOffset;
++ (NSArray*)createPixelsWithSize:(NSInteger)size
+						   color:(NSColor*)color
+							rows:(NSInteger)rows
+						 columns:(NSInteger)columns
+						 xOffset:(NSInteger)xOffset
+						 yOffset:(NSInteger)yOffset;
 
 @end
 
 #pragma mark -
 
 @implementation MysterioView
-
-- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
-{
-    self = [super initWithFrame:frame isPreview:isPreview];
-    if (self) {
-        [self setAnimationTimeInterval:1 / FRAME_RATE];
-
-		int width = self.bounds.size.width;
-		int height = self.bounds.size.height;
-
-		int pixelSize = MAX(width, height) / PIXEL_COUNT_MAX;
-
-		int rows = width / pixelSize;
-		int cols = height / pixelSize;
-
-		int xOffset = (width - pixelSize * rows) / 2;
-		int yOffset = (height - pixelSize * cols) / 2;
-
-		self.bluePixels = [MysterioView createPixelsWithSize:pixelSize
-													   color:[[NSColor blueColor] colorWithAlphaComponent:0.75]
-														rows:rows
-													 columns:cols
-													 xOffset:xOffset
-													 yOffset:yOffset];
-		self.whitePixels = [MysterioView createPixelsWithSize:pixelSize
-													   color:[[NSColor whiteColor] colorWithAlphaComponent:0.75]
-														rows:rows
-													 columns:cols
-													 xOffset:xOffset
-													 yOffset:yOffset];
-	}
-
-    return self;
-}
 
 + (NSArray*)createPixelsWithSize:(NSInteger)size color:(NSColor*)color rows:(NSInteger)rows columns:(NSInteger)columns xOffset:(NSInteger)xOffset yOffset:(NSInteger)yOffset
 {
@@ -88,45 +59,110 @@
 	return [NSArray arrayWithArray:rowArray];
 }
 
-- (void)startAnimation
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
 {
-    [super startAnimation];
-}
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1 / FRAME_RATE];
 
-- (void)stopAnimation
-{
-    [super stopAnimation];
+		int width = self.bounds.size.width;
+		int height = self.bounds.size.height;
+
+		int pixelSize = MAX(width, height) / PIXEL_COUNT_MAX;
+
+		int rows = width / pixelSize;
+		int cols = height / pixelSize;
+
+		int xOffset = (width - pixelSize * rows) / 2;
+		int yOffset = (height - pixelSize * cols) / 2;
+
+		self.bluePixels = [MysterioView createPixelsWithSize:pixelSize
+													   color:[[NSColor blueColor] colorWithAlphaComponent:0.0]
+														rows:rows
+													 columns:cols
+													 xOffset:xOffset
+													 yOffset:yOffset];
+		self.whitePixels = [MysterioView createPixelsWithSize:pixelSize
+														color:[[NSColor whiteColor] colorWithAlphaComponent:0.0]
+														rows:rows
+													 columns:cols
+													 xOffset:xOffset
+													 yOffset:yOffset];
+	}
+
+    return self;
 }
 
 - (void)drawRect:(NSRect)rect
 {
-    [super drawRect:rect];
+	[super drawRect:rect];
+
+	[[NSGraphicsContext currentContext] setCompositingOperation:NSCompositeCopy];
+	for (NSArray *column in self.bluePixels) {
+		for (MysterioPixel *pixel in column) {
+			[pixel.color set];
+			[pixel fill];
+		}
+	}
+
+	[[NSGraphicsContext currentContext] setCompositingOperation:NSCompositeOverlay];
+	for (NSArray *column in self.whitePixels) {
+		for (MysterioPixel *pixel in column) {
+			[pixel.color set];
+			[pixel fill];
+		}
+	}
 }
 
 - (void)animateOneFrame
 {
-	NSColor *black = [NSColor blackColor];
-
-	[black set];
-	[NSBezierPath fillRect:self.bounds];
-
 	for (NSArray *column in self.bluePixels) {
 		for (MysterioPixel *pixel in column) {
-			if (SSRandomIntBetween(0, 1) == 1) {
-				[pixel.color set];
-				[pixel fill];
+			CGFloat alpha = pixel.color.alphaComponent;
+
+			switch(SSRandomIntBetween(0, 2)) {
+				default:
+				case 0:
+					// No change
+					break;
+
+				case 1:
+					alpha -= 0.1;
+					break;
+
+				case 2:
+					alpha += 0.1;
+					break;
 			}
+
+			pixel.color = [pixel.color colorWithAlphaComponent:alpha];
 		}
 	}
 
 	for (NSArray *column in self.whitePixels) {
 		for (MysterioPixel *pixel in column) {
-			if (SSRandomIntBetween(0, 1) == 1) {
-				[pixel.color set];
-				[pixel fill];
+			CGFloat alpha = pixel.color.alphaComponent;
+
+			switch(SSRandomIntBetween(0, 2)) {
+				default:
+				case 0:
+					// No change
+					break;
+
+				case 1:
+					alpha -= 0.1;
+					break;
+
+				case 2:
+					alpha += 0.1;
+					break;
 			}
+
+			pixel.color = [pixel.color colorWithAlphaComponent:alpha];
 		}
 	}
+
+	[self setNeedsDisplay:YES];
 
     return;
 }
